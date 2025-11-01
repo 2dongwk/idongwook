8075ebaf7c4f16da989fd0a605ee585c271e7876
Dongwook Lee <skyldw3@gmail.com>
2025-11-01 21:53:31 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <main className="p-6">
       <h1 className="text-3xl font-bold tracking-tight md:text-5xl">
-        포트폴리오 v2 — Tailwind 적용완료
+        포트폴리오 v2 — Tailwind 적용완료 test
         <Button></Button>
       </h1>
     </main>
